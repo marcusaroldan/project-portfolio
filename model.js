e3d94422858a360ca1410de5ff8407e3b65cda50
roldanm02 <roldan.m@northeastern.edu>
2024-09-02 18:32:03 -0400

    created full transit map to contain all content of the site. Added new css class for map sections with associated styling

--- model.js
+++ model.js
@@ -1,79 +1,107 @@
 var nodeInfo, nodeArray, edgeArray, container, data, options;
 
+// NODE CONSTANTS
+const NODE_SIZE = 15;
+const NODE_COLOR = "#FFFFFF";
+const NODE_SHAPE = 'dot';
+
+// EDGE CONSTANTS
+const OL_COLOR = '#ed8b00';
+const GL_COLOR = '#00843d';
+const RL_COLOR = '#da291c';
+const EDGE_SMOOTH = {enabled: true, type: 'discrete', forceDirection: 'none', roundness: 0};
+const EDGE_SIZE = 15;
+
 function init_network() 
 {    
-    nodeInfo = {
-        'threeOneOne': {
-            id: 'threeOneOne',
-            html: '311',
-            color: 'colorHere',
-            relSize: 'sizeHere'
-        },
-        'BICRE': {
-            id: 'BICRE',
-            html: 'bicre',
-            color: 'colorHere',
-            relSize: 'sizeHere'
-        },
-        'MBTA': {
-            id: 'MBTA',
-            html: 'mbta-analysis',
-            color: 'colorHere',
-            relSize: 'sizeHere'
-        },
-        'ROVE': {
-            id: 'ROVE',
-            html: 'rove',
-            color: 'colorHere',
-            relSize: 'sizeHere'
-        },
-        'AI': {
-            id: 'AI',
-            html: 'ai-chord-prediction',
-            color: 'colorHere',
-            relSize: 'sizeHere'
-        }
+    nodeHTMLID= {
+        'threeOneOne': '311',
+        'BICRE-OL': 'bicre',
+        'BICRE-GL': 'bicre',
+        'MBTA': 'mbta-analysis',
+        'ROVE': 'rove',
+        'AI': 'ai-chord-prediction',
+        'intro': 'brief-intro',
+        'neu': 'northeastern-intro',
+        'wishroute': 'wishroute',
+        'trans': 'transportation',
+        'NL': 'netherlands',
+        'career-RL': 'career-path',
+        'career-GL': 'career-path',
+        'BCU-OLRL': 'BCU',
+        'BCU-GL': 'BCU',
+        'contact-RL': 'contact-info',
+        'contact-GL': 'contact-info'
     };
 
     nodeArray = [
-        {id: 'threeOneOne', label: '311 Infra Issue Identifier', color: "#FFFFFF", shape: 'dot', size: 10},
-        {id: 'BICRE', label: "BICRE", color: "#FFFFFF", shape: 'dot', size: 10},
-        {id: 'MBTA', label: 'MBTA Data Analysis', color: "#FFFFFF", shape: 'dot', size: 10},
-        {id: 'ROVE', label: 'ROVE', color: "#FFFFFF", shape: 'dot', size: 10},
-        {id: 'AI', label: 'AI Chord Recognition/Prediction Model', color: "#FFFFFF", shape: 'dot', size: 10} 
+        {id: 'threeOneOne', label: '311 Infra Issue Identifier', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 70, y:-300},
+
+        {id: 'BICRE-OL', label: "BICRE", color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 70, y: -100},
+        {id: 'BICRE-GL', label: "", color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 40, y: -100},
+
+        {id: 'MBTA', label: 'MBTA Data Analysis', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 270, y: 150},
+
+        {id: 'ROVE', label: 'ROVE', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 70, y: -200},
+
+        {id: 'AI', label: 'AI Chord Recognition/\nPrediction Model', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 290, y: 330},
+
+        {id: 'intro', label: 'About me', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: -230, y: -300},
+
+        {id: 'neu', label: 'Northeastern', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: -210, y: -200},
+
+        {id: 'wishroute', label: 'Wishroute, Inc.', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: -130, y: -130},
+
+        {id: 'trans', label: 'Transportation', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 370, y: -200},
+
+        {id: 'NL', label: 'Netherlands', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 310, y: -100},
+
+        {id: 'career-RL', label: 'Career Path', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: -10, y: 150},
+        {id: 'career-GL', label: '', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: -40, y: 150},
+
+        {id: 'BCU-OLRL', label: 'Boston Cyclist Union', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 70, y: 0},
+        {id: 'BCU-GL', label: '', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: 40, y: 0},
+
+        {id: 'contact-RL', label: 'Contact Info', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: -50, y: 300},
+        {id: 'contact-GL', label: '', color: NODE_COLOR, shape: NODE_SHAPE, size: NODE_SIZE, x: -80, y: 300},
     ];
 
     edgeArray = [
-        {from: 'ROVE', to: 'threeOneOne', color: '#ed8b00', background: {enabled: true, size: 15, color: '#ed8b00'}, length: 300, smooth: {enabled: true, type: 'discrete', forceDirection: 'none', roundness: 0}},
-        {from: 'threeOneOne', to: 'AI', color: '#ed8b00', background: {enabled: true, size: 15, color: '#ed8b00'}, length: 250, smooth: {enabled: true, type: 'discrete', forceDirection: 'none', roundness: 0}},
-        {from: 'threeOneOne', to: 'MBTA', color: '#00843d', background: {enabled: true, size: 15, color: '#00843d'}, length: 325, smooth: {enabled: true, type: 'discrete', forceDirection: 'none', roundness: 0}},
-        // {from: 'MBTA', to: 'BICRE', color: '#00843d', background: {enabled: true, size: 15, color: '#00843d'}, length: 190, smooth: {enabled: true, type: 'discrete', forceDirection: 'none', roundness: 0}},
-        {from: 'AI', to: 'BICRE', color: '#ed8b00', background: {enabled: true, size: 15, color: '#ed8b00'}, length: 200, smooth: {enabled: true, type: 'discrete', forceDirection: 'none', roundness: 0}}
+        // Orange Line
+        {from: 'threeOneOne', to: 'ROVE', color: OL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: OL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'ROVE', to: 'BICRE-OL', color: OL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: OL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'BICRE-OL', to: 'BCU-OLRL', color: OL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: OL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'BCU-OLRL', to: 'MBTA', color: OL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: OL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'MBTA', to: 'AI', color: OL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: OL_COLOR}, smooth:EDGE_SMOOTH},
+
+        // Green Line
+        {from: 'intro', to: 'neu', color: GL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: GL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'neu', to: 'wishroute', color: GL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: GL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'wishroute', to: 'BICRE-GL', color: GL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: GL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'BICRE-GL', to: 'BCU-GL', color: GL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: GL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'BCU-GL', to: 'career-GL', color: GL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: GL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'career-GL', to: 'contact-GL', color: GL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: GL_COLOR}, smooth: EDGE_SMOOTH},
+
+        // Red Line
+        {from: 'trans', to: 'NL', color: RL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: RL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'NL', to: 'BCU-OLRL', color: RL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: RL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'BCU-OLRL', to: 'career-RL', color: RL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: RL_COLOR}, smooth: EDGE_SMOOTH},
+        {from: 'career-RL', to: 'contact-RL', color: RL_COLOR, background: {enabled: true, size: EDGE_SIZE, color: RL_COLOR}, smooth: EDGE_SMOOTH},
     ];
 
     var nodes = new vis.DataSet(nodeArray);
     var edges = new vis.DataSet(edgeArray);
 
-    container = document.getElementById("network_map");
+    container = document.getElementById("transit-map");
     data = {
         nodes: nodes,
         edges: edges
     };
     options = {
-        layout: {randomSeed: '0.4354599665758836:1724964443030'},
-        physics: {
-            hierarchicalRepulsion: {
-                avoidOverlap: 1,},
-            repulsion: {
-                springConstant: 0.9,
-                centralGravity: 0.05,
-                nodeDistance: 200
-            },
-            solver: 'repulsion'
-        },
+        physics: false,
         nodes: {
             font: {
-                size: 35,
+                size: 30,
             }
         },
         interaction: {
@@ -87,20 +115,24 @@ function init_network()
 init_network();
 
 var network = new vis.Network(container, data, options);
-var lastNodeSelectionHTMLID = false;
+var lastNodeSelectionHTMLElement = document.getElementById('brief-intro');
 
 network.on('selectNode', function(params) {
     if (params.nodes.length === 1) {
-        if (lastNodeSelectionHTMLID !== false) document.getElementById(lastNodeSelectionHTMLID).style.display = 'none';
-        let selectedNode = params.nodes[0];
-        let htmlID = nodeInfo[selectedNode]['html'];
-        document.getElementById(htmlID).style.display = 'block';
-        lastNodeSelectionHTMLID = htmlID;
+        lastNodeSelectionHTMLElement.style.display = 'none';
+        let selectedNodeID = params.nodes[0];
+        let selectedNodeHTMLID = nodeHTMLID[selectedNodeID];
+        let selectedNodeHTML = document.getElementById(selectedNodeHTMLID);
+
+        selectedNodeHTML.style.display = 'block';
+
+        lastNodeSelectionHTMLElement = selectedNodeHTML;
     }
 });
 
 network.on('deselectNode', function(params) {
     if (params.previousSelection.nodes.length === 1) {
-        if (lastNodeSelectionHTMLID !== false) document.getElementById(lastNodeSelectionHTMLID).style.display = 'none';
+        lastNodeSelectionHTMLElement.style.display = 'none';
+        // document.getElementById('brief-intro').style.display = 'block';
     }
 });
